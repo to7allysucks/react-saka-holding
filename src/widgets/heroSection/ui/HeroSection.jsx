12e71d5d5a1fb731eb80828a40331ae613802907
unknown <elsukovdenis8@gmail.com>
2026-06-03 18:15:26 +0300

    feat: add slider on HomePage

--- src/widgets/heroSection/ui/HeroSection.jsx
+++ src/widgets/heroSection/ui/HeroSection.jsx
@@ -1,76 +1,97 @@
-import { Pagination } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { useRef } from 'react'
+import {Pagination} from 'swiper/modules'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {useRef, useState} from 'react'
 import styles from './HeroSection.module.scss'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
-import heroImg from '../../../shared/assets/images/hero-Slider/girl_with_cloth.svg'
+import heroImg
+  from '../../../shared/assets/images/hero-Slider/girl_with_cloth.svg'
 import arrowImg from '../../../shared/assets/icons/strelka_hero-slider.svg'
 
 const HeroSection = () => {
-    
-    const MOCK_SLIDERS =[
-        {
-            title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
-            img: heroImg,
-            bg: "green"
-        },
-        {
-            title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
-            img: heroImg,
-            bg: "purple"
 
-        },
-        {
-            title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
-            img: heroImg,
-            bg: "yellow"
+  const MOCK_SLIDERS = [
+    {
+      title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
+      img: heroImg,
+      bgLeft: "#325488",
+      bgRight: "#370470"
+    },
+    {
+      title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
+      img: heroImg,
+      bgLeft: "#FFAA00",
+      bgRight: "#BF8F30"
+    },
+    {
+      title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
+      img: heroImg,
+      bgLeft: "#33CCCC",
+      bgRight: "#009999"
+    },
+    {
+      title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
+      img: heroImg,
+      bgLeft: "#BF3030",
+      bgRight: "#FF7373"
+    },
+    {
+      title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
+      img: heroImg,
+      bgLeft: "#FFFF00",
+      bgRight: "#BFBF30"
+    },
+  ]
 
-        },
-        {
-            title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
-            img: heroImg,
-            bg: "red"
+  const [isActiveSlide, setIsActiveSlide] = useState(0)
 
-        },
-        {
-            title: 'Здесь будет слайдер с различными акциями или специальными предложениями',
-            img: heroImg,
-            bg: "gray"
+  const swiperRef = useRef(null)
 
-        },
-    ]
+  return (
+    <div className={styles.heroWrapper}>
+      <Swiper
+        modules={[Pagination]}
+        className={styles.slider}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+          el: `.${styles.sliderPagination}`,
+          renderBullet: (index, className) => {
+            return `<span class="${className}"></span>`;
+          }
+        }}
+        onSlideChange={(swiper) => {
+          setIsActiveSlide(swiper.realIndex)
+        }}
+      >
 
-    const swiperRef = useRef(null)
+        {MOCK_SLIDERS.map(slide => (
+          <SwiperSlide className={styles.slide}
+                       style={{background: `linear-gradient(-45deg, ${slide.bgLeft} 50%, ${slide.bgRight} 50%)`}}>
+            <div className={styles.content}>
+              <h2>{slide.title}</h2>
+              <button className={styles.btnHero}>Подробнее <img src={arrowImg}
+                                                                alt="btn"/>
+              </button>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className={styles.paginationWrapper}>
+        <div className={styles.sliderPagination}></div>
 
-    return (
-        <div className={styles.heroWrapper}>
-            <Swiper
-                modules={[Pagination]} 
-                className={styles.slider}
-                onSwiper={ (swiper) => (swiperRef.current = swiper) }
-                slidesPerView={1}
-                Pagination= {{
-                        clickable: true,
-                        el: '.sliderPagination'
-                        // renderBullet: (index, className) => {return '<span class="' + className + '">' + (index + 1) + "</span>";}
-                    }}
-                    >
+        <span className={styles.counter}>
+          {String(isActiveSlide + 1).padStart(2, '0')}
+          {' / '}
+          {String(MOCK_SLIDERS.length).padStart(2, '0')}
+        </span>
+      </div>
+      <img src={heroImg} alt="heroimg"/>
 
-                {MOCK_SLIDERS.map( slide  => (
-                    <SwiperSlide className={styles.slide} style={{backgroundColor: slide.bg}}>
-                        <div className={styles.content}>
-                            <h2>{slide.title}</h2>
-                            <button className={styles.btnHero}>Подробнее <img src={arrowImg} alt="btn" /></button>
-                            <div className={styles.sliderPagination}></div>
-                        </div>
-                        <img src={slide.img} alt="heroimg" />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default HeroSection

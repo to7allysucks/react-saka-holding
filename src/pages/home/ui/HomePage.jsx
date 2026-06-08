79aa89f167008fd6894da648754dc15b544061f5
to7allysucks <to7allysucks@gmail.com>
2026-06-09 02:16:53 +0300

    feat: add layout news-card

--- src/pages/home/ui/HomePage.jsx
+++ src/pages/home/ui/HomePage.jsx
@@ -1,12 +1,14 @@
 import CalculatePriceSection from '../../../widgets/calculatePriceSection/ui/CalculatePriceSection'
 import Certificates from '../../../widgets/certificatesSection/ui/Certificates'
 import HeroSection from '../../../widgets/heroSection/ui/HeroSection'
+import NewsCard from '../../../shared/ui/news-card/NewsCard'
 import styles from './HomePage.module.scss'
 
 const HomePage = () => {
     return (
         <div className={styles.home}>
             <HeroSection />
+            <NewsCard />
             <Certificates />
             <CalculatePriceSection />
         </div>
---
+++ src/shared/ui/news-card/NewsCard.jsx
@@ -0,0 +1,27 @@
+import styles from './NewsCard.module.scss';
+import arrowIcon from '../../../shared/assets/icons/arrow_hero-slider.svg';
+
+const NewsCard = (props) => {
+    const { image, title, description, date, link, className } = props
+
+    return (
+        <a href={link || '#'} className={`${styles.newsCard} ${className || ''}`}>
+            <div className={`${styles.imageWrapper}`}>
+                <img src={image} alt={title} className={`${styles.image}`} />
+                <div className={`${styles.overlay}`}></div>
+            </div>
+
+            <div className={`${styles.arrowButton}`}>
+                <img src={arrowIcon} alt="arrow" />
+            </div>
+
+            <div className={`${styles.content}`}>
+                <h3 className={`${styles.title}`}>{title}</h3>
+                <p className={`${styles.description}`}>{description}</p>
+                <span className={`${styles.date}`}>{date}</span>
+            </div>
+        </a>
+    )
+}
+
+export default NewsCard;

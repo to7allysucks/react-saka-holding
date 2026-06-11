18fd267d54c044b5ad246d1ddfe646e452673b37
Kwinxxx <elsukovdenis8@gmail.com>
2026-06-11 12:10:25 +0300

    feat: add model price-calc-section

--- src/shared/ui/buttons/gold-btn/GoldBtn.jsx
+++ src/shared/ui/buttons/gold-btn/GoldBtn.jsx
@@ -1,11 +1,17 @@
 import styles from './GoldBtn.module.scss'
 
 const GoldBtn = (props) => {
-    const {type, img, children, className} = props
+    const {type='button',
+        img,
+        children,
+        className,
+        classNameImg,
+    } = props
 
     return (
-        <button className={`${className} ${styles.button}`}>{children}<img src={img}
-                                                        alt="btn"/>
+        <button className={`${className} ${styles.button}`}>
+            {children}
+            <img src={img} alt="btn" className={classNameImg}/>
         </button>
     )
 }
--- src/widgets/calculate-price-section/ui/CalculatePriceSection.jsx
+++ src/widgets/calculate-price-section/ui/CalculatePriceSection.jsx
@@ -1,11 +1,40 @@
 import CalculatorCloth from '../../../shared/ui/calculator'
+import GoldBtn from '../../../shared/ui/buttons/gold-btn'
 import styles from './CalculatePriceSection.module.scss'
+import plusImg from '../../../shared/assets/icons/modular-windows/X_gray.svg'
+import { useState } from 'react'
 
 const CalculatePriceSection = () => {
+
+    // [isCard, setIsCard] = useState(1)
+
+    const calcPrice = () => {
+        
+    }
+
     return (
         <div className={styles.container}>
             <h3 className={styles.title}>Рассчитайте стоимость ткани, ответив на три вопроса</h3>
-            <CalculatorCloth />
+            <div className={styles.cardsWrapper}> 
+                <CalculatorCloth />
+                <GoldBtn img={plusImg} classNameImg={styles.imgParams}>Добавить товар</GoldBtn>
+                <div className={styles.resultWrapper}>
+                    <div className={styles.resultScore}>
+                        <div>
+                            <span>Общее количество рулонов:</span>
+                            <div>10</div>
+                        </div>
+                        <div>
+                            <span>Общее количество пачек:</span>
+                            <div>24</div>
+                        </div>
+                    </div>
+                    <div className={styles.resultScore}>
+                        <span>Итоговая сумма за все позиции:</span>
+                        <div className={styles.resultPrice}>100 245 $</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
